55998a13cb10c4691842bcaedb808594c1dfcf0e
Hugo Lanzafame <hugo.lanzafame@ynov.com>
2024-01-26 21:02:28 +0100

    No entry translation

--- src/components/custom/CustomPopupDelete.jsx
+++ src/components/custom/CustomPopupDelete.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dialog, DialogTitle, DialogContent, DialogActions, Button, Box} from '@mui/material';
+import {Dialog, DialogTitle, DialogContent, DialogActions, Button} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 import PropTypes from "prop-types";
